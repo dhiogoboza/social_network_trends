cf2c2494785d56ff23edf7826b62be1f0ac7fb1f
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2017-12-30 02:11:17 -0300

    Moved useful scripts to 'api' folder
    Removed some unused files

--- twitter.sample.py
+++
@@ -1,5 +0,0 @@
-# Copy this file, rename to twitter.py and put your twitter keys and tokens bellow:
-API_KEY = "YOUR API_KEY"
-API_SECRET = "YOUR API_SECRET"
-ACCESS_TOKEN = "YOUR ACCESS_TOKEN"
-ACCESS_TOKEN_SECRET = "YOUR ACCESS_TOKEN_SECRET"
