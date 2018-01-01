e2ed2ad24ac7ad7da25333797502d23b080ce271
Ruben O. Chiavone <ruben.ochiavone@gmail.com>
2018-01-01 13:24:50 -0300

    Improved model indexing and serialization

--- api/trend_history.py
+++ api/trend_history.py
@@ -7,9 +7,11 @@ class TrendHistory(ndb.Model):
     """A model for representing history of trends search by place"""
     date = ndb.DateProperty(indexed=True)
     trends = ndb.StructuredProperty(Trend, repeated=True)
-    location = ndb.StructuredProperty(Location, repeated=False)
+    location = ndb.StructuredProperty(Location, repeated=False, indexed=True)
 
     def to_dict(self):
-        dict = super(TrendHistory, self).to_dict()
+        dict = super(TrendHistory, self).to_dict(exclude=['date'])
         dict['as_of'] = self.date.strftime('%Y-%m-%dT%H:%M:%SZ')
+        dict['locations'] = [dict['location']]
+        del dict['location']
         return dict
